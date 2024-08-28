c86d201c2775642057f858e4a38a6f06b40c22c7
TheNorthRem <1371002466@qq.com>
2024-08-28 23:35:40 +0800

    force update

---
+++ bn-logger-service/src/main/java/com/bupt/bnlogger/BnLoggerAppApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.bnlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnLoggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnLoggerAppApplication.class, args);
+	}
+
+}
