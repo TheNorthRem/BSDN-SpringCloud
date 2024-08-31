d9d240c79795a0e4dbd39527a0ee09b7aacd719d
Jungle <1239946358@qq.com>
2024-08-31 21:37:51 +0800

    kafka消费者

--- bn-logger-service/src/main/java/com/bupt/bnlogger/BnLoggerAppApplication.java
+++ bn-logger-service/src/main/java/com/bupt/bnlogger/BnLoggerAppApplication.java
@@ -2,12 +2,14 @@ package com.bupt.bnlogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class BnLoggerAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BnLoggerAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BnLoggerAppApplication.class, args);
+    }
 
 }
