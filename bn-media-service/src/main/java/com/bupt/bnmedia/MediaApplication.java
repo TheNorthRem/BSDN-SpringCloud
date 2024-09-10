58e902a4acf1c91e9672029b7aac6c11a66f427a
liyehui02 <1371002466@qq.com>
2024-09-10 18:56:16 +0800

    media 服务注册

---
+++ bn-media-service/src/main/java/com/bupt/bnmedia/MediaApplication.java
@@ -0,0 +1,17 @@
+package com.bupt.bnmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @Author liyehui02
+ * @Date 2024/9/10 下午2:36
+ */
+@SpringBootApplication
+public class MediaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaApplication.class, args);
+    }
+
+}
