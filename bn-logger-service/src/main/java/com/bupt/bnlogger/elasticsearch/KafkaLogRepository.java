e033157d2152cb9c21065407371b45e212c01ad1
Jungle <1239946358@qq.com>
2024-09-12 18:20:43 +0800

    防止target文件提交

---
+++ bn-logger-service/src/main/java/com/bupt/bnlogger/elasticsearch/KafkaLogRepository.java
@@ -0,0 +1,7 @@
+package com.bupt.bnlogger.Elasticsearch;/** 
+* @Description: TODO
+* @author: scott
+* @date: 2024年09月12日 17:42
+*/
+public class KafkaLogRepository {
+}
---
+++ bn-logger-service/src/test/java/LogServiceEsTest.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;/** 
+* @Description: TODO
+* @author: scott
+* @date: 2024年09月12日 18:05
+*/
+public class LogServiceEsTest {
+}
